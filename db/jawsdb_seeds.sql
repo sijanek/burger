dc30a982b2babe3c54bcf5d9ab45929e5e6f48b4
Ragamuffin <ivana.l@cox.net>
2019-07-30 00:02:47 -0700

    adding JAWSDB

--- db/jawsdb_seeds.sql
+++
@@ -1,6 +0,0 @@
-USE;
-
-INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Baconator", true);
-
